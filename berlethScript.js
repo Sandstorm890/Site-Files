3b73c9b452d460b5a60f2c9372fb8d53a8a69567
zaynegoering <zaynegoering@gmail.com>
2024-02-05 13:45:50 -0800

    refactor/add funtions for image scroll

--- berlethScript.js
+++ berlethScript.js
@@ -6,87 +6,68 @@
       const thumbnailBar = document.getElementById("thumbnailBar");
       const captionRightElement = document.getElementById("caption-right");
 
-      let currentIndex = 0
-
-      const thumbnailsPerPage = 14;
-      let currentPage = 0;
-
+      let featuredPhotoIndex = 0;
+      
       function showThumbnails() {
-        const start = currentPage * thumbnailsPerPage;
-        const end = start + thumbnailsPerPage;
-
+        console.log("featuredPhotoIndex:", featuredPhotoIndex)
         thumbnailBar.innerHTML = '';
+        const startIndex = (featuredPhotoIndex - 6) < 0 ? 0 : featuredPhotoIndex - 6;
+        const endIndex = (featuredPhotoIndex + 6) > imageListWithCaptions.length ? imageListWithCaptions.length - 1 : featuredPhotoIndex + 6;
 
-        for (let i = start; i < end && i < imageListWithCaptions.length; i++) {
-          const imgElement = document.createElement("img");
+        console.log("startIndex: ", startIndex);
+        console.log("endIndex: ", endIndex);
+
+
+        for (let i = startIndex; i <= endIndex; i++) {
+          const imgElement = document.createElement('img');
           imgElement.src = `${imageDirectory}/${imageListWithCaptions[i].image}`;
           imgElement.alt = imageListWithCaptions[i].image.replace(/\.[^/.]+$/, "");
           imgElement.onclick = function () {
-            currentIndex = i
+            
             updateFeaturedImage(i);
           };
 
           thumbnailBar.appendChild(imgElement);
         }
-      }
+      };
+
+      function featuredImageForward() {
+        const nextIndex = (featuredPhotoIndex + 1)
+        updateFeaturedImage(nextIndex);
+        showThumbnails()
+      };
+
+      function featuredImageBack() {
+        const nextIndex = (featuredPhotoIndex - 1)
+        updateFeaturedImage(nextIndex);
+        showThumbnails()
+      };
+      
 
       function updateFeaturedImage(index) {
         featuredImage.src = `${imageDirectory}/${imageListWithCaptions[index].image}`;
         captionRightElement.textContent = imageListWithCaptions[index].caption;
-      }
-
-      showThumbnails();
-      updateFeaturedImage(0);
-
-      const prevButton = document.getElementById("prevButton");
-      const nextButton = document.getElementById("nextButton");
-
-      // prevButton.addEventListener("click", function () {
-      //   if (currentPage > 0) {
-      //     currentPage--;
-      //     showThumbnails();
-      //   }
-      // });
-
-      // nextButton.addEventListener("click", function () {
-      //   const totalPages = Math.ceil(imageListWithCaptions.length / thumbnailsPerPage);
-      //   if (currentPage < totalPages - 1) {
-      //     currentPage++;
-      //     showThumbnails();
-      //   }
-      // });
-
+        featuredPhotoIndex = index
+        showThumbnails()
+      };
+    
       document.addEventListener('keydown', function(event) {
         try {
           if (event.key === 'ArrowRight') {
-              const nextIndex = (currentIndex + 1)
-              updateFeaturedImage(nextIndex);
-              currentIndex++
-            } else if (event.key === 'ArrowLeft') {
-              const nextIndex = (currentIndex - 1)
-              updateFeaturedImage(nextIndex);
-              currentIndex--
-            }
+            featuredImageForward()
+          } else if (event.key === 'ArrowLeft') {
+            featuredImageBack()
+          }
         } catch {console.log("Reached end of photo list")}
-        });
-      
-
-      // featuredImageContainer.addEventListener("keypress", (event) => {
-      //   console.log("Arrow listener function init")
-      //   if (event.key === 'ArrowRight') {
-      //     const nextIndex = (currentIndex + 1)
-      //     currentIndex++
-      //     updateFeaturedImage(nextIndex);
-      //   }
-      // });
+      });
       
       featuredImageContainer.addEventListener("click", function () {
-        console.log("Click listener init")
-        const nextIndex = (currentIndex + 1)
-        currentIndex++
-        updateFeaturedImage(nextIndex);
+        featuredImageForward()
       });
 
+      showThumbnails();
+      updateFeaturedImage(0);
+      
     });
 
 
