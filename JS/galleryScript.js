f4a74d14463665ba29302d2d08831edb771d8ca6
zaynegoering <zaynegoering@gmail.com>
2024-02-13 13:56:05 -0800

    change thumbnail bar to scroll on fixed featured photo index

--- JS/galleryScript.js
+++ JS/galleryScript.js
@@ -7,7 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const captionRightElement = document.getElementById("caption-right");
   const ironwoodBtn = document.getElementById("ironwood-btn")
 
-  let featuredPhotoIndex = 0;
+  let featuredPhotoIndex = imageListWithCaptions.length - 8;
+  console.log(featuredPhotoIndex)
   
   function showThumbnails() {
 
@@ -21,9 +22,11 @@ document.addEventListener("DOMContentLoaded", function () {
       imgElement.onclick = function () {
         updateFeaturedImage(imageListWithCaptions.findIndex((image) => image == element))
       }
+      
       if (element == imageListWithCaptions[featuredPhotoIndex]) {
         imgElement.style.height = "30px"
         imgElement.style.width = "80px"
+        console.log(imgElement)
       }
 
       thumbnailBar.appendChild(imgElement);
@@ -52,31 +55,40 @@ document.addEventListener("DOMContentLoaded", function () {
     showThumbnails()
   };
 
-  function updateFeaturedImage(index) {
-    console.log(featuredImage)
-    featuredImage.src = `${imageDirectory}/${imageListWithCaptions.at(index).image}`;
+  function updateFeaturedImage() {
+    featuredImage.src = `${imageDirectory}/${imageListWithCaptions.at(featuredPhotoIndex).image}`;
     featuredImage.style.animation = "fadeInAnimation ease 1.5s"
-    captionRightElement.textContent = imageListWithCaptions.at(index).caption;
-    featuredPhotoIndex = index
+    captionRightElement.textContent = imageListWithCaptions.at(featuredPhotoIndex).caption;
+    // featuredPhotoIndex = index
     showThumbnails()
   };
 
   document.addEventListener('keydown', function(event) {
     
       if (event.key === 'ArrowRight') {
-        featuredImageForward()
+        
+        imageListWithCaptions.push(imageListWithCaptions.shift())
+        showThumbnails()
+        updateFeaturedImage()
+        console.log("Right press")
+        // featuredImageForward()
+
       } else if (event.key === 'ArrowLeft') {
-        featuredImageBack()
+
+        imageListWithCaptions.unshift(imageListWithCaptions.pop())
+        showThumbnails()
+        updateFeaturedImage()
+        // featuredImageBack()
       }
     
   });
   
   featuredImageContainer.addEventListener("click", function () {
-    featuredImageForward()
+    // featuredImageForward()
   });
 
   showThumbnails();
-  updateFeaturedImage(0);
+  updateFeaturedImage();
 
   
   
